517f6d05be546c9a26e5dd2b1df01d2d68cf1918
Emile Bl <bailay.emile@live.fr>
2020-05-20 10:30:04 +0200

    models created

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ mongoose.connect('mongodb+srv://emilebl:lourd_666@cluster0-3zvt3.gcp.mongodb.net
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
